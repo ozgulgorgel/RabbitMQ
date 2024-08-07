dae804d10c8093b394f8423fcdb3c646ac776561
özgül görgel <ozgulgrgl@outlook.com>
2024-08-07 21:45:24 +0300

    Add project files.

---
+++ Consumer.App/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Consumer.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //baglantı oluşturma
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://qqtnrdtf:yRNoGuuL1LEGKLk2JlWdYvNRQCf8LGxy@puffin.rmq2.cloudamqp.com/qqtnrdtf");
+
+            //baglantı aktifleştirme ve kanal acma
+            using IConnection connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+          
+            channel.QueueDeclare(queue: "fanout-queue",durable:true, exclusive: false,autoDelete:false,arguments:null);
+            channel.QueueBind("fanout-queue", exchange: "fanout-exchange", routingKey: string.Empty, arguments: null);
+
+            //queue dan mesaj okuma
+            EventingBasicConsumer consumer = new(channel);
+            channel.BasicConsume(queue: "fanout-queue", false, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                //kuyruğa gelen mesajın işlendiği yerdir
+
+                //e.Body:kuyruktaki mesajın verisini bütünsel olrak getircektir
+                //e.Body.AsSpan veya e.Body.ToaARRAY() :Kuyruktaki mesajın byte verisini getirecektir
+                Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+            };
+            Console.Read();
+        }
+    }
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Publisher.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://qqtnrdtf:yRNoGuuL1LEGKLk2JlWdYvNRQCf8LGxy@puffin.rmq2.cloudamqp.com/qqtnrdtf");
+            //baglantıyı aktifleştirme ve kanal acma
+            using IConnection connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            channel.BasicReturn += (sender, e) =>
+            {
+                var messagee = Encoding.UTF8.GetString(e.Body.ToArray());
+                Console.WriteLine($"message ilgili  kuyruğa gönderilemedi.{messagee}");
+
+            };
+
+            //fanout exchange
+            channel.ExchangeDeclare("fanout-exchange", ExchangeType.Fanout, durable: true, autoDelete: false, arguments: null);
+
+
+            for (int i = 0; i < 11; i++)
+            {
+                var message = $"message {i}";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "fanout-exchange", routingKey:string.Empty,mandatory:true,null,messageBody);
+            }
+           
+
+            Console.WriteLine("message gönderildi");
+
+            Console.Read();
+        }
+    }
+}
